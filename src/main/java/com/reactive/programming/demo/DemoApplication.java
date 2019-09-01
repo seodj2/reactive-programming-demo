715fabb3c4b2dc1ef6667d1134ce8aef162e601b
dongjin.seo <dongjin.seo@nhnent.com>
2019-09-01 22:57:19 +0900

    deleted AsyncConfigurer

--- src/main/java/com/reactive/programming/demo/DemoApplication.java
+++ src/main/java/com/reactive/programming/demo/DemoApplication.java
@@ -2,10 +2,9 @@ package com.reactive.programming.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 
 @SpringBootApplication
-public class DemoApplication implements AsyncConfigurer {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
